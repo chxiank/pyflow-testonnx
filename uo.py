1a189fae0c596c74a01bc05a317844626449603f
cxk <530597318@qq.com>
2024-04-19 13:37:29 +0800

    new

--- uo.py
+++ uo.py
@@ -1,2 +1,14 @@
 print(888)
 import numpy as np
+
+def uo(data, n1=7, n2=14, n3=28, n4=56):
+    data = np.array(data)
+    data = data.reshape(-1, 1)
+    data = data.astype(np.float64)
+    data = data.tolist()
+    uo = []
+    for i in range(len(data)):
+        if i < n1 + n2 + n3 + n4:
+            uo.append(0)
+        else:
+            uo.append(())
