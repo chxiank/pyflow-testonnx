864c2a7e9d577026b7786ec6d41f06b60dcaf115
cxk <530597318@qq.com>
2024-04-19 13:38:44 +0800

    new2

--- uo.py
+++ uo.py
@@ -12,3 +12,8 @@ def uo(data, n1=7, n2=14, n3=28, n4=56):
             uo.append(0)
         else:
             uo.append(())
+
+            uo[i] = (data[i] - min(data[i - n1:i])) / (max(data[i - n1:i]) - min(data[i - n1:i])) * 100
+            uo[i] = (uo[i] - min(uo[i - n2:i])) / (max(uo[i - n2:i]) - min(uo[i - n2:i])) * 100
+            uo[i] = (uo[i] - min(uo[i - n3:i])) / (max(uo[i - n3:i]) - min(uo[i - n3:i])) * 100
+            
